c246bcb1f9ac309e2597b0166cfa4223daa9f298
Mahziyar <mhzrerfani@gmail.com>
2025-10-21 08:53:44 +0330

    fix: Update CORS origin from localhost:3010 to localhost:3000

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'http://localhost:3010',
+      'http://localhost:3000',
       'https://news-feed-monorepo-vue-nest-api.vercel.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
